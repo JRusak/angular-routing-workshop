90b1570cbb298f7350f0a3326f33e85cb8d6d5b5
Jan Rusak <jrusak001@gmail.com>
2023-07-28 11:40:04 +0200

    Done

--- src/app/watch/watch.component.ts
+++ src/app/watch/watch.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-watch',
   templateUrl: './watch.component.html',
-  styleUrls: ['./watch.component.css']
+  styleUrls: ['./watch.component.css'],
 })
 export class WatchComponent {
+  constructor(private readonly route: ActivatedRoute) {}
 
+  id: number | null = null;
+
+  async ngOnInit() {
+    const paramSub = this.route.params.subscribe((params) => {
+      this.id = params['id'];
+    });
+  }
 }
